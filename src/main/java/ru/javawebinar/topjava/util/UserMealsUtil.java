f13ec81c5f984055314edf540e2dfb7e264c657e
IPS19 <i89214228815@gmail.com>
2023-01-15 22:30:24 +0300

    implement filteredByStreams() and filteredByCycles()

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -25,27 +25,24 @@ public class UserMealsUtil {
         System.out.println(filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
         System.out.println(filteredByCycles(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
 
-
     }
 
-
-
     public static List<UserMealWithExcess> getMealsWithExcess(List<UserMeal> meals, int caloriesPerDay) {
-        List<UserMealWithExcess> resultList = new ArrayList<>();//список, в который вносится еда с превышением калорий
-        List<UserMealWithExcess> dailyList = new ArrayList<>();//список с едой за один день
-        LocalDate singleDay = null; //для разделения по дням
+        List<UserMealWithExcess> resultList = new ArrayList<>();
+        List<UserMealWithExcess> dailyList = new ArrayList<>();
+        LocalDate singleDay = null;
 
         int sumCalories = 0;
         for (int i = 0; i < meals.size(); i++) {
             UserMeal meal = meals.get(i);
-            if (singleDay == null) { //первый заход по списку
-                singleDay = meal.getDateTime().toLocalDate(); //в разделитель по дням записываем дату первого элемента списка
-                dailyList.add(meal.convertToMealWithExcess()); //первый элемент записываем в первый элемент за день
+            if (singleDay == null) {
+                singleDay = meal.getDateTime().toLocalDate();
+                dailyList.add(meal.convertToMealWithExcess());
                 sumCalories += meal.getCalories(); //суммируем калории
 
-            } else if (singleDay.equals(meal.getDateTime().toLocalDate())) { //если дата дня та же, что и в  след.элементе списка (this.еда-meal)
-                dailyList.add(meal.convertToMealWithExcess()); //то добавляем ее в список с едой за один день
-                sumCalories += meal.getCalories();//суммируем калории
+            } else if (singleDay.equals(meal.getDateTime().toLocalDate())) {
+                dailyList.add(meal.convertToMealWithExcess());
+                sumCalories += meal.getCalories();
                 if (meals.size() == i + 1 && (sumCalories > caloriesPerDay)) {
                     resultList.addAll(dailyList);
                 }
@@ -70,27 +67,24 @@ public class UserMealsUtil {
         for (UserMealWithExcess meal : mealsWithExcess) {
             LocalTime mealTime = meal.getDateTime().toLocalTime();
 
-            if (TimeUtil.isBetweenHalfOpen(mealTime,startTime,endTime)) {
+            if (TimeUtil.isBetweenHalfOpen(mealTime, startTime, endTime)) {
                 mealsInInterval.add(meal);
             }
-
         }
         return mealsInInterval;
     }
 
-
     public static List<UserMealWithExcess> filteredByStreams(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        Map<LocalDate, List<UserMeal>> groupedMealsByDate = meals.stream().collect(Collectors.groupingBy(UserMeal::getLocalDate));//группировка по дням в мапу
+        Map<LocalDate, List<UserMeal>> groupedMealsByDate = meals.stream().collect(Collectors.groupingBy(UserMeal::getLocalDate));
 
-        return groupedMealsByDate.values()//взяли значения мапы
+        return groupedMealsByDate.values()
                 .stream()
-                .filter(x -> (x.stream().mapToInt(UserMeal::getCalories)).sum() > caloriesPerDay).flatMap(Collection::stream).collect(Collectors.toList())//фильтруем - оставляем только значения с превышением калорий и пишем в коллекцию
-                .stream().filter(x->{//фильтруем получившуюся коллекцию по дате
+                .filter(x -> (x.stream().mapToInt(UserMeal::getCalories)).sum() > caloriesPerDay).flatMap(Collection::stream).collect(Collectors.toList())
+                .stream().filter(x -> {
                     LocalTime mealTime = x.getDateTime().toLocalTime();
                     return mealTime.isAfter(startTime) && mealTime.isBefore(endTime);
                 })
                 .collect(Collectors.toList())
-                .stream().map(UserMeal::convertToMealWithExcess).collect(Collectors.toList());//преобразовываем в MealWithExcess
-
+                .stream().map(UserMeal::convertToMealWithExcess).collect(Collectors.toList());
     }
 }
