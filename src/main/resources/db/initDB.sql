676d56588eaf482c8817e5ce4ce7645d3f61c9ee
IPS19 <i89214228815@gmail.com>
2023-02-19 22:02:50 +0300

    add and populate meals table, add setters and default constructor, implement JdbcMealRepository

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -23,3 +23,14 @@ CREATE TABLE user_role
     CONSTRAINT user_roles_idx UNIQUE (user_id, role),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE TABLE meals
+(
+    id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    date_time   TIMESTAMP NOT NULL,
+    description TEXT NOT NULL,
+    calories    INTEGER NOT NULL,
+    user_id     INT NOT NULL,
+    FOREIGN KEY (user_id) references users (id) ON DELETE CASCADE,
+    CONSTRAINT meal_datetime_idx UNIQUE (date_time)
+);
