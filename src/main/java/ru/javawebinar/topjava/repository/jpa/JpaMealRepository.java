3483cc2a6306761ee7baf646515fde31e70c7d57
IPS19 <i89214228815@gmail.com>
2023-02-26 22:57:52 +0300

    implement JpaMealRepository, tests ok, except deleteNotFound

--- src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
@@ -1,6 +1,11 @@
 package ru.javawebinar.topjava.repository.jpa;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
@@ -12,22 +17,37 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
+@Transactional
 public class JpaMealRepository implements MealRepository {
+
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    private Session openSession() {
+        return sessionFactory.getCurrentSession();
+    }
 
     @PersistenceContext
     private EntityManager em;
 
     @Override
     public Meal save(Meal meal, int userId) {
+        User user = getRefUser(userId);
         if (meal.isNew()) {
-            User user = em.getReference(User.class, userId);
             meal.setUser(user);
             em.persist(meal);
             return meal;
         } else {
-            User user = meal.getUser();
-            if (user.getId() == userId) {
-                em.merge(meal);
+            if (em.createQuery("UPDATE Meal m SET m.calories=:calories, m.dateTime=:dateTime, m.description=:description WHERE m.id=:id AND m.user=:user")
+                    .setParameter("calories", meal.getCalories())
+                    .setParameter("dateTime", meal.getDateTime())
+                    .setParameter("description", meal.getDescription())
+                    .setParameter("id", meal.getId())
+                    .setParameter("user", user)
+                    .executeUpdate() != 0) {
+                meal.setUser(user);
+                return meal;
             }
         }
         return null;
@@ -44,16 +64,26 @@ public class JpaMealRepository implements MealRepository {
 
     @Override
     public Meal get(int id, int userId) {
-        return em.find(Meal.class, id);
+        User user = getRefUser(userId);
+        List<Meal> meals = em.createQuery("SELECT m FROM Meal m WHERE m.id=:id AND m.user=:user")
+                .setParameter("id", id).setParameter("user", user).getResultList();
+        return DataAccessUtils.singleResult(meals);
+    }
+
+    private User getRefUser(int id) {
+        return em.getReference(User.class, id);
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        Query query = em.createQuery("SELECT m FROM Meal m WHERE m.user.id=:userId ORDER BY m.dateTime DESC");
+        return query.setParameter("userId", userId).getResultList();
     }
 
     @Override
     public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        return null;
+        return em.createQuery("SELECT m FROM Meal m WHERE m.user.id=?1 AND m.dateTime>=?2 AND m.dateTime<?3 ORDER BY m.dateTime DESC")
+                .setParameter(1, userId).setParameter(2, startDateTime).setParameter(3, endDateTime)
+                .getResultList();
     }
 }
